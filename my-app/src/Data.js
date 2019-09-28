49db4274f93bef4bf00e16ca73dc49b2026771a0
issa ouedraogo <i.ouedraogo49@yahoo.fr>
2019-09-28 09:04:30 -0400

    Com

--- my-app/src/Data.js
+++ my-app/src/Data.js
@@ -4,6 +4,11 @@ import './App.css';
 import {Card, CardHeader, CardFooter, CardBody,
     CardTitle, CardText, Button} from 'reactstrap';
 import FooterComponent from './header_footer/footer';
+import Age from "./Age.png";
+import Gender from "./Gender.png";
+import Language from "./Language.png";
+import UserID from "./UserID.png";
+import { Agent } from 'http';
 
 class Data extends Component {
     constructor(props) {
@@ -13,7 +18,25 @@ class Data extends Component {
         return (
             <div className="App">
                 <NavigationMenu/>
-                <p>INSERT DATA STUFF HERE</p>
+                    <div className="age_graph">
+                        <img src={Age} />
+                    </div>
+                    <br/>
+                    <div className="gender_graph">
+                        <img src={Gender} />
+                    </div>
+                    <br/>
+                    <div className="language_graph">
+                        <img src={Language} />
+                    </div>
+                    <br/>
+                    <div className="user_graph">
+                        <img src={UserID} />
+                    </div>
+                    <br/>
+                    {/* <div className="age_graph">
+                        <img src={} />
+                    </div> */}
                 <CardFooter>
                     <FooterComponent/>
                 </CardFooter>
