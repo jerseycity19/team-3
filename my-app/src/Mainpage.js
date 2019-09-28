5908a50266d28ab26629849a79d7d309e9ae0ab5
abhatt349 <abhatt349@gmail.com>
2019-09-28 07:57:25 -0400

    fixed button

--- my-app/src/Mainpage.js
+++ my-app/src/Mainpage.js
@@ -9,7 +9,8 @@ class Mainpage extends Component {
         return (
             <div>
                 Many scholars around the world face the serious issue of self-censorship, in which they’re unable to freely express thoughts and ideas for fear of persecution. We’re asking for your help in combating this extremely detrimental effect by simply filling out our survey. You’ll remain completely anonymous, and this data will help us fight against self-censorship worldwide.
-                <button>#put the informed consent button here</button>
+								I understand and agree that the data I submit will be used anonymously in reports, studies, and advocacy about self-censorship.
+                <button href="/Data">I understand and agree</button>
             </div>
         );
     }
--- sentimentAnalysis.py
+++ sentimentAnalysis.py
@@ -29,45 +29,45 @@ stop_words = list(set(stopwords.words('english')))
 allowed_word_types = ["J"]
 
 for p in  files_pos:
-    
+
     # create a list of tuples where the first element of each tuple is a review
     # the second element is the label
     documents.append( (p, "pos") )
-    
+
     # remove punctuations
     cleaned = re.sub(r'[^(a-zA-Z)\s]','', p)
-    
-    # tokenize 
+
+    # tokenize
     tokenized = word_tokenize(cleaned)
-    
-    # remove stopwords 
+
+    # remove stopwords
     stopped = [w for w in tokenized if not w in stop_words]
-    
-    # parts of speech tagging for each word 
+
+    # parts of speech tagging for each word
     pos = nltk.pos_tag(stopped)
-    
+
     # make a list of  all adjectives identified by the allowed word types list above
     for w in pos:
         if w[1][0] in allowed_word_types:
             all_words.append(w[0].lower())
-   
+
   for p in files_neg:
     # create a list of tuples where the first element of each tuple is a review
     # the second element is the label
     documents.append( (p, "neg") )
-    
+
     # remove punctuations
     cleaned = re.sub(r'[^(a-zA-Z)\s]','', p)
-    
-    # tokenize 
+
+    # tokenize
     tokenized = word_tokenize(cleaned)
-    
-    # remove stopwords 
+
+    # remove stopwords
     stopped = [w for w in tokenized if not w in stop_words]
-    
-    # parts of speech tagging for each word 
+
+    # parts of speech tagging for each word
     neg = nltk.pos_tag(stopped)
-    
+
     # make a list of  all adjectives identified by the allowed word types list above
     for w in neg:
         if w[1][0] in allowed_word_types:
@@ -80,7 +80,7 @@ all_words = nltk.FreqDist(all_words)
 word_features = list(all_words.keys())[:5000]
 
 # function to create a dictionary of features for each review in the list document.
-# The keys are the words in word_features 
+# The keys are the words in word_features
 # The values of each key are either true or false for wether that feature appears in the review or not
 
 def find_features(document):
@@ -93,7 +93,7 @@ def find_features(document):
 # Creating features for each review
 featuresets = [(find_features(rev), category) for (rev, category) in documents]
 
-# Shuffling the documents 
+# Shuffling the documents
 random.shuffle(featuresets)
 
 training_set = featuresets[:20000]
@@ -110,7 +110,7 @@ from sklearn.naive_bayes import MultinomialNB,BernoulliNB
 from sklearn.linear_model import LogisticRegression,SGDClassifier
 from sklearn.svm import SVC
 
-# training various models by passing in the sklearn models into the SklearnClassifier from NLTK 
+# training various models by passing in the sklearn models into the SklearnClassifier from NLTK
 
 MNB_clf = SklearnClassifier(MultinomialNB())
 MNB_clf.train(training_set)
@@ -132,22 +132,22 @@ ground_truth = [r[1] for r in testing_set]
 predictions = {}
 f1_scores = {}
 f
-or clf, listy in classifiers_dict.items(): 
+or clf, listy in classifiers_dict.items():
     # getting predictions for the testing set by looping over each reviews featureset tuple
-    # The first elemnt of the tuple is the feature set and the second element is the label 
+    # The first elemnt of the tuple is the feature set and the second element is the label
     predictions[clf] = [listy[0].classify(r[0]) for r in testing_set]
     f1_scores[clf] = f1_score(ground_truth, predictions[clf])
     print(f'f1_score {clf}: {f1_scores[clf]}')
 
     from nltk.classify import ClassifierI
 
-# Defininig the ensemble model class 
+# Defininig the ensemble model class
 
 class EnsembleClassifier(ClassifierI):
-    
+
     def __init__(self, *classifiers):
         self._classifiers = classifiers
-    
+
     # returns the classification based on majority of votes
     def classify(self, features):
         votes = []
@@ -155,7 +155,7 @@ class EnsembleClassifier(ClassifierI):
             v = c.classify(features)
             votes.append(v)
         return mode(votes)
-    # a simple measurement the degree of confidence in the classification 
+    # a simple measurement the degree of confidence in the classification
     def confidence(self, features):
         votes = []
         for c in self._classifiers:
@@ -166,10 +166,10 @@ class EnsembleClassifier(ClassifierI):
         conf = choice_votes / len(votes)
         return conf
 
-        # Load all classifiers from the pickled files 
+        # Load all classifiers from the pickled files
 
 # function to load models given filepath
-def load_model(file_path): 
+def load_model(file_path):
     classifier_f = open(file_path, "rb")
     classifier = pickle.load(classifier_f)
     classifier_f.close()
@@ -179,23 +179,23 @@ def load_model(file_path):
 # Original Naive Bayes Classifier
 ONB_Clf = load_model('pickled_algos/ONB_clf.pickle')
 
-# Multinomial Naive Bayes Classifier 
+# Multinomial Naive Bayes Classifier
 MNB_Clf = load_model('pickled_algos/MNB_clf.pickle')
 
 
-# Bernoulli  Naive Bayes Classifier 
+# Bernoulli  Naive Bayes Classifier
 BNB_Clf = load_model('pickled_algos/BNB_clf.pickle')
 
-# Logistic Regression Classifier 
+# Logistic Regression Classifier
 LogReg_Clf = load_model('pickled_algos/LogReg_clf.pickle')
 
 # Stochastic Gradient Descent Classifier
 SGD_Clf = load_model('pickled_algos/SGD_clf.pickle')
 
-# Initializing the ensemble classifier 
+# Initializing the ensemble classifier
 ensemble_clf = EnsembleClassifier(ONB_Clf, MNB_Clf, BNB_Clf, LogReg_Clf, SGD_Clf)
 
-# List of only feature dictionary from the featureset list of tuples 
+# List of only feature dictionary from the featureset list of tuples
 feature_list = [f[0] for f in testing_set]
 
 # Looping over each to classify each review
@@ -203,4 +203,4 @@ ensemble_preds = [ensemble_clf.classify(features) for features in feature_list]
 
 def sentiment(text):
     feats = find_features(text)
-    return ensemble_clf.classify(feats), ensemble_clf.confidence(feats)    
+    return ensemble_clf.classify(feats), ensemble_clf.confidence(feats)
