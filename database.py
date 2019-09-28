e99600147d2e5581c48445b1a2bf99b505d09825
abhatt349 <abhatt349@gmail.com>
2019-09-28 06:51:19 -0400

    added nlp stuff

--- database.py
+++ database.py
@@ -22,6 +22,10 @@ def meme(a):
 def metric(x1, x2,  x3,  x4,  x5,  x6,  x7):
     return sum(x1/3, x2/3, x3/3, meme(x5)/2, meme(x6)/2, meme(x7)/2)
 
+adjectives = {'happy', 'scared', 'fearful', 'safe', 'free', 'sad', 'angry', 'resentful', 'excited', 'jubilant', 'consternated', 'elated', 'blank'}
+
+
+
 list = [(choose(4), choose(6), choose(3), rand.choice(("Syria", "USA", "Canada", "Germany", "Turkey")),
          rand.choice(("English", "French", "German", "Arabic", "Turkish")), choose(5), choose(9), choose(4),
          choose(4), choose(4), choose(4), choose(8), choose(3), choose(3), choose(3)) for _ in range(100)]
