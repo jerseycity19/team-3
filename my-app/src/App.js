03389cb3599ed765b46927696723524f8b364f74
LeoAuyeung <leo297747@gmail.com>
2019-09-28 00:10:55 -0400

    Add initial translation with i18next

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,40 +1,115 @@
-import React from 'react';
+import React, { Component, Suspense } from 'react';
+import { useTranslation, withTranslation, Trans } from 'react-i18next';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+// use hoc for class based components
+class LegacyWelcomeClass extends Component {
+  render() {
+    const { t, i18n } = this.props;
+    return (
+      <h2>{t('title')}YERRRRRRRS</h2>
+    );
+  }
+}
+const Welcome = withTranslation()(LegacyWelcomeClass);
+
+class InputForm extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      id: "",
+      agerange: "",
+    }
+  }
+
+  handleChange = (event) => {
+    this.setState({
+        [event.target.name]: event.target.value
+    });
+  }
+
+  // handleRadioChange = (event) => {
+  //   this.setState({
+  //       [event.target.name]: event.target.value
+  //   });
+  // }
+
+  render() {
+    const { t, i18n } = this.props;
+    const { id, agerange } = this.state;
+
+    return (
+      <div>
+        <h2>{t('title')}YERRRRRRRS</h2>
+        <form>
+          {/* <label>
+            User Identification:
+          <input type="text" name="name" />
+          </label> */}
+          <Trans>
+          {t('id')}
+          <input type="text" name="name" onChange={this.handleChange}>{id}</input>
+          </Trans>
+          <Trans>
+          {t('age-range')}
+          <input type="text" name="name" onChange={this.handleChange}>{agerange}</input>
+          </Trans>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    );
+  }
+}
+const Form = withTranslation()(InputForm);
+
+// Component using the Trans component
+function MyComponent() {
+  return (
+    <Trans i18nKey="description.part1">
+      To get started, edit <code>src/App.js</code> and save to reload.
+    </Trans>
+  );
+}
+
+// page uses the hook
+function Page() {
+  const { t, i18n } = useTranslation();
+
+  const changeLanguage = lng => {
+    i18n.changeLanguage(lng);
+  };
+
   return (
     <div className="App">
-      <h1> Please let us know your experience</h1>
-      <form>
-        <label>
-          User Iden:
-          <input type="text" name="name" />
-        </label>
-        <input type="submit" value="Submit" />
-        <label>
-          Name:
-          <input type="text" name="name" />
-        </label>
-        <input type="submit" value="Submit" />
-        <label>
-          Name:
-          <input type="text" name="name" />
-        </label>
-        <input type="submit" value="Submit" />
-        <label>
-          Name:
-          <input type="text" name="name" />
-        </label>
-        <input type="submit" value="Submit" />
-        <label>
-          Name:
-          <input type="text" name="name" />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Welcome />
+        <button onClick={() => changeLanguage('de')}>de</button>
+        <button onClick={() => changeLanguage('en')}>en</button>
+      </div>
+      <div className="App-intro">
+        <MyComponent />
+      </div>
+      <Form t={t} i18n={i18n}/>
+      <div>{t('description.part2')}</div>
     </div>
   );
 }
 
-export default App;
+// loading component for suspense fallback
+const Loader = () => (
+  <div className="App">
+    <img src={logo} className="App-logo" alt="logo" />
+    <div>loading...</div>
+  </div>
+);
+
+// here app catches the suspense from page in case translations are not yet loaded
+export default function App() {
+  return (
+    <Suspense fallback={<Loader />}>
+      <Page />
+    </Suspense>
+  );
+}
