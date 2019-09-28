654362154d0b12f725e2d55f68af5426fd134102
issa ouedraogo <i.ouedraogo49@yahoo.fr>
2019-09-27 20:40:16 -0400

    Initial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Please let us know your experience</h1>
+      <form>
+        <label>
+          User Iden:
+          <input type="text" name="name" />
+        </label>
+        <input type="submit" value="Submit" />
+        <label>
+          Name:
+          <input type="text" name="name" />
+        </label>
+        <input type="submit" value="Submit" />
+        <label>
+          Name:
+          <input type="text" name="name" />
+        </label>
+        <input type="submit" value="Submit" />
+        <label>
+          Name:
+          <input type="text" name="name" />
+        </label>
+        <input type="submit" value="Submit" />
+        <label>
+          Name:
+          <input type="text" name="name" />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+}
+
+export default App;
